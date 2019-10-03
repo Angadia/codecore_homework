5aad22191d910c02047f5235d997b406d3867d81
Jignesh Patel <ca.jignesh.angadia@gmail.com>
2019-10-02 22:59:13 -0700

    Added stretch solution to read the command line inputs.

--- turtle_graphics/turtle.js
+++ turtle_graphics/turtle.js
@@ -31,7 +31,7 @@ class Turtle {
         let highestX = this.pointsVisited.map(el => el).reduce(((max,cur) => Math.max(max, cur[0])), 0);
         let highestY = this.pointsVisited.map(el => el).reduce(((max,cur) => Math.max(max, cur[1])), 0);
         for (let y = lowestY, row = ''; y <= highestY; y++, row='') {
-            for (let x = lowestX, status = '='; x <= highestX; x++, status='=') {
+            for (let x = lowestX, status = ' '; x <= highestX; x++, status=' ') {
                 this.pointsVisited.forEach(point => {
                     if (point[0] == x && point[1] == y) status = '+';
                 });
@@ -41,18 +41,40 @@ class Turtle {
         };
     };
 };
-new Turtle(0, 4)
-.forward(3)
-.left()
-.forward(3)
-.right()
-.forward(5)
-.right()
-.forward(8)
-.right()
-.forward(5)
-.right()
-.forward(3)
-.left()
-.forward(3)
-.print();
+
+function processTurtleCmds (cmds, turtle) {
+    cmds.forEach(cmd => {
+        if (!cmd.startsWith('f') && !cmd.startsWith('r') && !cmd.startsWith('l')) {
+            console.log(`Skipping unrecognized cmd \'${cmd}\'.`);
+        } else {
+            if (cmd.startsWith('f')) {
+                turtle.forward(parseInt(cmd.slice(1)));
+            } else if (cmd.startsWith('r')) {
+                turtle.right();
+            } else {
+                turtle.left();
+            };
+        };
+    });
+};
+
+if (process.argv.length < 4) {
+    let cmds = process.argv[2].split('-');
+    let flash;
+    if (cmds.length == 0) {
+        flash = new Turtle(0, 0);
+    } else if (!cmds[0].startsWith('t')) {
+        flash = new Turtle(0, 0);
+    } else {
+        cmd = cmds.shift();
+        point = cmd.slice(1).split(',');
+        if (point.length != 2 || !parseInt(point[0]) || !parseInt(point[1])) {
+            console.log('Please provide commands in required format.');
+            return;
+        } else {
+            flash = new Turtle(parseInt(point[0]), parseInt(point[1]));
+        };
+    };
+    processTurtleCmds(cmds, flash);
+    flash.print();
+};
